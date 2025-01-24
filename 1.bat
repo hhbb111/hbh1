693fb7cb42e657c7ebdd9bbaf9d6db08912cac57
hhbb111 <moahanaa3@gmail.com>
2025-01-25 06:13:14 +0700

    Add files via upload

---
+++ 1.bat
@@ -0,0 +1,6 @@
+@cd/d "C:\LDPlayer\LDPlayer4.0"
+@cd/d "D:\LDPlayer\LDPlayer4.0"
+@cd/d "E:\LDPlayer\LDPlayer4.0"
+@cd/d "F:\LDPlayer\LDPlayer4.0"
+@cd/d "Y:\LDPlayer\LDPlayer4.0"
+dnconsole launch --index 0
