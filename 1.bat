5fafe48357a15d992881eb33f8e0d0a3eb244616
hhbb111 <moahanaa3@gmail.com>
2025-01-25 06:14:44 +0700

    Delete 1.bat

--- 1.bat
+++
@@ -1,6 +0,0 @@
-@cd/d "C:\LDPlayer\LDPlayer4.0"
-@cd/d "D:\LDPlayer\LDPlayer4.0"
-@cd/d "E:\LDPlayer\LDPlayer4.0"
-@cd/d "F:\LDPlayer\LDPlayer4.0"
-@cd/d "Y:\LDPlayer\LDPlayer4.0"
-dnconsole launch --index 0
