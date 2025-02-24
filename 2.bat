c960b183df0b2cd7883044d13842b92cbf18d7f4
hhbb111 <moahanaa3@gmail.com>
2025-02-25 04:22:52 +0700

    Add files via upload

---
+++ 2.bat
@@ -0,0 +1,12 @@
+@echo off
+adb kill-server
+adb root
+
+
+
+:loop
+adb.exe shell rm -rf /data/data/com.tencent.ig/files/ano_tmp/ano_emu_c2.dat");
+    adb.exe shell rm -rf /data/data/com.tencent.ig/files
+  
+TIMEOUT 1
+goto:loop
