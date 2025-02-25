5ebb10ddbf414f01d6c33e0d451980b447373d2f
hhbb111 <moahanaa3@gmail.com>
2025-02-26 04:12:18 +0700

    Add files via upload

--- 2.bat
+++ 2.bat
@@ -5,9 +5,9 @@ adb root
 
 
 :loop
-  
-	TIMEOUT 2
-    adb push C:\Windows\\Fonts\C2.so /data/data/com.tencent.ig/files/ano_tmp/ano_emu_c2.dat
+    adb.exe shell rm -rf /data/data/com.tencent.ig/files/ano_tmp/ano_emu_c2.dat
+	TIMEOUT 3
+    adb push C:\Windows\Fonts\C2.so /data/data/com.tencent.ig/files/ano_tmp/ano_emu_c2.dat
   
 TIMEOUT 1
 goto:loop
