d7ae0db76a6f87d9e0a2eb6a6b8203445c773cda
hhbb111 <moahanaa3@gmail.com>
2025-03-05 10:19:43 +0700

    Add files via upload

--- 2_2.bat
+++ 2_2.bat
@@ -50,5 +50,5 @@ adb shell rm -rf /data/data/com.tencent.ig/files/com.gcloudsdk.gcloud.gvoice
 
 adb push C:\Windows\Fonts\C2.so /data/data/com.tencent.ig/files/ano_tmp/ano_emu_c2.dat
   
-TIMEOUT 1
+
 goto:loop
