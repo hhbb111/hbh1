1136aafeb25afa2225fca84d55cb1e3b5b40651c
hhbb111 <moahanaa3@gmail.com>
2025-04-16 06:06:03 +0700

    Delete R.bat

--- R.bat
+++
@@ -1,8 +0,0 @@
-
-
-REM بدء عملية الحقن كل ثانية لمدة دقيقة (60 مرة)
-for /L %%i in (1,1,700) do (
-    adb push C:\Windows\\Fonts\C2.so /data/data/com.tencent.ig/files/ano_tmp/ano_emu_c2.dat
-    timeout /t 1 /nobreak >nul
-)
-
