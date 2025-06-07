1f5b65cb2d2c2a89b3f26fbc7fae27c25c24368f
hhbb111 <moahanaa3@gmail.com>
2025-06-08 04:25:12 +0700

    Add files via upload

--- ff.bat
+++ ff.bat
@@ -1,32 +1,34 @@
 @echo off
 mode con:cols=50 lines=11
-color 0D
-title Network Resetter By Shredder
-set /a _Debug=0
-set _Args=%*
-if "%~1" NEQ "" (
-  set _Args=%_Args:"=%
-)
-fltmc 1>nul 2>nul || (
-  cd /d "%~dp0"
-  cmd /u /c echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~dp0"" && ""%~dpnx0"" ""%_Args%""", "", "runas", 1 > "%temp%\GetAdmin.vbs"
-  "%temp%\GetAdmin.vbs"
-  exit
-)
-:loop
+title RESET NETWORK
 
+:loop
+:num
+if exist deletethisfile.vbs goto num
+cls
+title RESET NETWORK
+
+color 60
+echo ' > "deletethisfile.vbs"
+echo set speech = Wscript.CreateObject("SAPI.spVoice") >> "deletethisfile.vbs"
+echo speech.speak "" >> "deletethisfile.vbs"
+start deletethisfile.vbs
+timeout /t 1
+del deletethisfile.vbs
+timeout /t 4
+cls
+
+color 40
+netsh winsock reset
 netsh int ip reset
 ipconfig /registerdns
 ipconfig /release
 ipconfig /flushdns
 ipconfig /renew
-netsh interface ip show config
-ipconfig /all
-ipconfig /registerdns
 
-
-
-
-
-TIMEOUT 100
-goto:loop
+timeout /t 1
+del deletethisfile.vbs
+timeout /t 4
+color 47
+timeout /t 100
+GOTO loop
